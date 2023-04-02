445fbcd4f0bd86f1bef5afc35b3df8bb4b75caba
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-04-02 23:12:54 +0700

    redirect after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/setup/magento?hide_nav_bar=true';
 
     /**
      * Create a new controller instance.
